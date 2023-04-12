38bd1aa16385695c8c4b593481026d7f56561a23
a.frajnd@iq-adv.ru <a.frajnd@iq-adv.ru>
2023-04-12 14:23:47 +0500

    plug in axios and pinia

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+definePageMeta({
+  layout: 'default',
+});
+</script>
+
+<template>
+  <main>Some sample of Nuxt 3 with Pinia and axios</main>
+</template>
+
+<style module></style>
